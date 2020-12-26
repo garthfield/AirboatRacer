0b1478a49c3026e25f9ffcd36194511e7e3fc5c6
garthfield <garth@joltplay.com>
2020-12-26 19:55:36 +0000

    Committed missing ar_powerup.cpp

---
+++ mp/src/game/server/airboatracer/ar_powerup.cpp
@@ -0,0 +1,53 @@
+#include "cbase.h"
+
+// memdbgon must be the last include file in a .cpp file!!!
+#include "tier0/memdbgon.h"
+
+class CAR_Powerup : public CBaseAnimating
+{
+public:
+	DECLARE_CLASS(CAR_Powerup, CBaseAnimating);
+	DECLARE_DATADESC();
+	void Precache(void);
+	void Spawn(void);
+	virtual void StartTouch(CBaseEntity *pOther);
+};
+
+LINK_ENTITY_TO_CLASS(race_powerup, CAR_Powerup);
+
+BEGIN_DATADESC(CAR_Powerup)
+DEFINE_ENTITYFUNC(StartTouch),
+END_DATADESC()
+
+void CAR_Powerup::Precache()
+{
+	BaseClass::Precache();
+	PrecacheModel("models/pickup/pickup.mdl");
+}
+
+void CAR_Powerup::Spawn(void)
+{
+	BaseClass::Spawn();
+
+	DevMsg("Powerup spawn\n");
+	Precache();
+
+	// Start the client-side animation of the power-up
+	UseClientSideAnimation();
+	SetPlaybackRate(1.0);
+	SetModel("models/pickup/pickup.mdl");
+	ResetSequence(LookupSequence("Idle"));
+
+	// Set physics trigger flags
+	AddSolidFlags(FSOLID_NOT_SOLID | FSOLID_TRIGGER);
+	VPhysicsInitNormal(SOLID_BBOX, GetSolidFlags(), false);
+}
+
+void CAR_Powerup::StartTouch(CBaseEntity *pOther)
+{
+	if (stricmp(pOther->GetClassname(), "prop_vehicle_airboat") == 0) {
+		DevMsg("POWERUP TOUCH: %s\n", pOther->GetClassname());
+	}
+
+	BaseClass::StartTouch(pOther);
+}
