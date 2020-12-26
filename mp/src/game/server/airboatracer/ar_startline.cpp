c0ed8640d8d3e61b6c4e475e4589ba0b030cd1a6
garthfield <garth@joltplay.com>
2020-12-26 17:17:23 +0000

    https://sourcesdk.wordpress.com/2020/12/26/13-laps-winners/
    Basic logic for counting laps and detecting a race winner.

--- mp/src/game/server/airboatracer/ar_startline.cpp
+++ mp/src/game/server/airboatracer/ar_startline.cpp
@@ -1,39 +1,41 @@
 #include "cbase.h"
-#include "triggers.h"
+#include "ar_startline.h"
 
-class CAR_StartlineEntity : public CTriggerMultiple
-{
-public:
-	DECLARE_CLASS(CAR_StartlineEntity, CTriggerMultiple);
-	DECLARE_DATADESC();
-
-	void Spawn();
-	void StartTouch(CBaseEntity *pOther);
-	void SetPlayerCheckpoint(int iPlayerIndex, int iCheckpoint);
-
-private:
-	int m_iPlayerCheckpoint[MAX_PLAYERS];   // Stores each player's current checkpoint
-};
+// NOTE: This has to be the last file included!
+#include "tier0/memdbgon.h"
 
 LINK_ENTITY_TO_CLASS(r_startline, CAR_StartlineEntity);
 
 BEGIN_DATADESC(CAR_StartlineEntity)
+DEFINE_KEYFIELD(m_iLastCheckpoint, FIELD_INTEGER, "lastCheck"),
 END_DATADESC()
+
+ConVar ar_laps("race_laps", "3", FCVAR_REPLICATED | FCVAR_NOTIFY, "Set the number of laps each race is", true, 1, false, 0);
 
 void CAR_StartlineEntity::Spawn()
 {
-	DevMsg("Spawned Startline\n");
+	DevMsg("Spawned Startline (Last checkpoint: %i)\n", m_iLastCheckpoint);
 	BaseClass::Spawn();
 }
 
 void CAR_StartlineEntity::StartTouch(CBaseEntity *pOther)
 {
 	if (stricmp(pOther->GetClassname(), "player") == 0) {
-		DevMsg("CAR_StartlineEntity::StartTouch() entity:%s\n", pOther->GetClassname());
+		if (m_iPlayerCheckpoint[pOther->entindex() - 1] == m_iLastCheckpoint) {
+			m_iPlayerLaps[pOther->entindex() - 1]++;
+			m_iPlayerCheckpoint[pOther->entindex() - 1] = 0;
+
+			if (m_iPlayerLaps[pOther->entindex() - 1] == ar_laps.GetInt()) {
+				DevMsg("Race Finished. Player: %d wins\n", pOther->entindex());
+			}
+		}
+		DevMsg("CAR_StartlineEntity::StartTouch() entity:%s Laps: %d\n", pOther->GetClassname(), m_iPlayerLaps[pOther->entindex()-1]);
 	}
 }
 
-void CAR_StartlineEntity::SetPlayerCheckpoint(int iPlayerIndex, int iCheckpoint) {
-	m_iPlayerCheckpoint[iPlayerIndex] = iCheckpoint;
-	DevMsg("Player %d checkpoint %d\n", iPlayerIndex, iCheckpoint);
+void CAR_StartlineEntity::SetPlayerCheckpoint(int iPlayerEntityIndex, int iCheckpoint) {
+	if (iCheckpoint == (m_iPlayerCheckpoint[iPlayerEntityIndex - 1] + 1)) {
+		m_iPlayerCheckpoint[iPlayerEntityIndex - 1] = iCheckpoint;
+		DevMsg("Player %d checkpoint %d\n", iPlayerEntityIndex, iCheckpoint);
+	}
 }
--- mp/src/game/server/airboatracer/ar_startline.h
+++ mp/src/game/server/airboatracer/ar_startline.h
@@ -3,12 +3,25 @@
 #pragma once
 
 #include "cbase.h"
+#include "triggers.h"
 
-class CAR_StartlineEntity {
+extern ConVar ar_laps;
+
+class CAR_StartlineEntity : public CTriggerMultiple
+{
 public:
+	DECLARE_CLASS(CAR_StartlineEntity, CTriggerMultiple);
+
 	void Spawn(void);
 	void StartTouch(CBaseEntity *pOther);
 	void SetPlayerCheckpoint(int iPlayerIndex, int iCheckpoint);
+
+private:
+	int m_iPlayerCheckpoint[MAX_PLAYERS]; // Stores each player's current checkpoint
+	int m_iLastCheckpoint;                // What's the number of the last checkpoint
+	int m_iPlayerLaps[MAX_PLAYERS];       // Stores each player's laps completed
+
+	DECLARE_DATADESC();
 };
 
 #endif // CAR_STARTLINE_H
